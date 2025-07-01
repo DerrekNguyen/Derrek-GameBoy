ac5028b3a212798237593d19b3948f997c80dde5
DerrekNguyen <mnguye21@kent.edu>
2025-07-01 15:24:12 -0400

    tests added

--- Derrek-GameBoy/Program.cs
+++ Derrek-GameBoy/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EmuTests;
+using System;
 
 class Program
 {
---
+++ Derrek-GameBoy/tests/check_gbe.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EmuTests
+{
+   public class CPUTests
+   {
+      [SetUp]
+      public void Setup()
+      {
+
+      }
+
+      [Test]
+      public void Test()
+      {
+         bool result = CPU.CPU_Step();
+         Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsFalse(result);
+      }
+   }
+}
