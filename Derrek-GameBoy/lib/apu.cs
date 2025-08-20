c1e2b83d42d6ca7613574cdc4ac6b69b2b0a23d1
DerrekNguyen <mnguye21@kent.edu>
2025-08-19 21:39:35 -0400

    Still trying to figure out. Starting on Pulse Waves on channel 1 + 2 soon hopefully

--- Derrek-GameBoy/lib/apu.cs
+++ Derrek-GameBoy/lib/apu.cs
@@ -22,14 +22,45 @@ public static class SquareChannel1
          case 0xFF13: return NR13;
 
          // Return bit 6 (length enable), 2, 1 and 0 (LS3B of period value)
-         case 0xFF14: return (byte)(NR14 & 0x47); 
+         case 0xFF14: return (byte)(NR14 & 0x47);
 
          default: return 0xFF;
+      } 
+   }
+
+   public static void Write(UInt16 address, byte value)
+   {
+      switch (address)
+      {
+         case 0xFF10:
+            NR10 = value;
+            break;
+
+         case 0xFF11:
+            NR11 = value;
+            break;
+
+         case 0xFF12:
+            NR12 = value;
+            break;
+
+         case 0xFF13:
+            NR13 = value;
+            break;
+
+         case 0xFF14:
+            NR14 = value;
+            break;
       }
    }
 }
 
 public static class APU
 {
+   public static void FrequencySweep()
+   {
+      byte unit = SquareChannel1.Read(0xFF10);
 
+
+   }
 }
