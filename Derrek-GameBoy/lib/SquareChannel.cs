a24bfa724efd2f6345828b9fb7d6c7ee3325d11f
DerrekNguyen <mnguye21@kent.edu>
2025-08-20 22:19:59 -0400

    Little refactoring + duty cycles. Still figuring it out

---
+++ Derrek-GameBoy/lib/SquareChannel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+public abstract class SquareChannel
+{
+   // Duty   Waveform    Ratio
+   // -------------------------
+   // 0      00000001    12.5%
+   // 1      10000001    25%
+   // 2      10000111    50%
+   // 3      01111110    75%
+   public byte[] dutyCycles = new byte[4]
+   {
+      0b00000001,
+      0b10000001,
+      0b10000111,
+      0b01111110
+   };
+
+   public abstract byte Read(UInt16 address);
+   public abstract void Write(UInt16 address, byte value);
+}
+
+public class SquareChannel1 : SquareChannel
+{
+   private byte NR10, NR11, NR12, NR13, NR14;
+   public override byte Read(UInt16 address)
+   {
+      switch (address)
+      {
+         case 0xFF10: return NR10;
+         case 0xFF11: return NR11;
+         case 0xFF12: return NR12;
+         case 0xFF13: return NR13;
+
+         // Return bit 6 (length enable), 2, 1 and 0 (LS3B of period value)
+         case 0xFF14: return (byte)(NR14 & 0x47);
+
+         default: return 0xFF;
+      }
+   }
+
+   public override void Write(UInt16 address, byte value)
+   {
+      switch (address)
+      {
+         case 0xFF10:
+            NR10 = value;
+            break;
+
+         case 0xFF11:
+            NR11 = value;
+            break;
+
+         case 0xFF12:
+            NR12 = value;
+            break;
+
+         case 0xFF13:
+            NR13 = value;
+            break;
+
+         case 0xFF14:
+            NR14 = value;
+            break;
+      }
+   }
+}
--- Derrek-GameBoy/lib/apu.cs
+++ Derrek-GameBoy/lib/apu.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection.Metadata.Ecma335;
 
 public enum ChannelType
 {
@@ -9,57 +10,13 @@ public enum ChannelType
    N_CHANNELS
 }
 
-public static class SquareChannel1
-{
-   private static byte NR10, NR11, NR12, NR13, NR14;
-   public static byte Read(UInt16 address)
-   {
-      switch (address)
-      {
-         case 0xFF10: return NR10;
-         case 0xFF11: return NR11;
-         case 0xFF12: return NR12;
-         case 0xFF13: return NR13;
-
-         // Return bit 6 (length enable), 2, 1 and 0 (LS3B of period value)
-         case 0xFF14: return (byte)(NR14 & 0x47);
-
-         default: return 0xFF;
-      } 
-   }
-
-   public static void Write(UInt16 address, byte value)
-   {
-      switch (address)
-      {
-         case 0xFF10:
-            NR10 = value;
-            break;
-
-         case 0xFF11:
-            NR11 = value;
-            break;
-
-         case 0xFF12:
-            NR12 = value;
-            break;
-
-         case 0xFF13:
-            NR13 = value;
-            break;
-
-         case 0xFF14:
-            NR14 = value;
-            break;
-      }
-   }
-}
-
 public static class APU
 {
+   private static SquareChannel Channel1 = new SquareChannel1();
+
    public static void FrequencySweep()
    {
-      byte unit = SquareChannel1.Read(0xFF10);
+      byte unit = Channel1.Read(0xFF10);
 
 
    }
